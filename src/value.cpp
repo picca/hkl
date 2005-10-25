866d6ddfebfefb728c87d2d82774297650d8deeb
picca <picca>
2005-10-25 15:58:16 +0000

    * update implementation of the Value class

--- src/value.cpp
+++ src/value.cpp
@@ -7,65 +7,63 @@ namespace hkl {
     {}
 
   Value::Value(std::string const & name, double value)
-    : Object(name)
-    {
-      set_value(value);
-    }
+    : Object(name),
+    m_value(value)
+    {}
 
   Value::Value(Value const & value)
-    : Object(value.get_name())
-    {
-      set_value(value.get_value());
-    }
+    : Object(value.get_name()),
+    m_value(value.m_value)
+    {}
 
   Value::~Value(void)
   {}
 
 
   bool
-    Value::operator == (Value const & value) const
-    {
-      return Object::operator==(value)
-        && fabs(get_value() - value.get_value()) < constant::math::epsilon_1;
-    }
+  Value::operator == (Value const & value) const
+  {
+    return Object::operator==(value)
+      && fabs(m_value - value.m_value) < constant::math::epsilon_1;
+  }
 
   Value &
-    Value::operator += (Value const & value)
-    {
-      set_value(get_value() + value.get_value());
-      return *this;
-    }
+  Value::operator += (Value const & value)
+  {
+    m_value += value.m_value;
+    return *this;
+  }
 
   Value &
-    Value::operator -= (Value const & value)
-    {
-      set_value(get_value() - value.get_value());
-      return *this;
-    }
+  Value::operator -= (Value const & value)
+  {
+    m_value -= value.m_value;
+    return *this;
+  }
 
   Value &
-    Value::operator /= (double const & d)
-    {
-      set_value(get_value() / d);
-      return *this;
-    }
+  Value::operator /= (double const & d)
+  {
+    m_value /= d;
+    return *this;
+  }
 
   Value &
-    Value::operator *= (double const & d)
-    {
-      set_value(get_value() * d);
-      return *this;
-    }
+  Value::operator *= (double const & d)
+  {
+    m_value *= d;
+    return *this;
+  }
 
   std::ostream & 
-    Value::printToStream(std::ostream & flux) const
-    {  
-      Object::printToStream(flux);
+  Value::printToStream(std::ostream & flux) const
+  {  
+    Object::printToStream(flux);
 
-      flux  << " Value: " << get_value();
+    flux  << " Value: " << m_value << std::endl;
 
-      return flux;
-    }
+    return flux;
+  }
 
 } // namespace hkl
 
