5ea3753e54c4bc929d56aa43db0edb2bb641ea2a
picca <picca>
2006-01-11 13:44:01 +0000

    * refactoring of bksys

---
+++ bksys/linux2/detect_cppunit.py
@@ -0,0 +1,18 @@
+# Frederic-Emmanuel PICCA, 2006
+
+import os
+import SCons.Util
+
+CLVar = SCons.Util.CLVar
+	
+def detect(env):
+	if env.has_key('CPPUNIT_LIBPATH') and env.has_key('CPPUNIT_CPPPATH'):
+		env.Append(LIBS = ['cppunit', 'dl'])
+	
+	cppunit_config = env.WhereIs('cppunit-config')
+	if cppunit_config:	
+		env.ParseConfig(cppunit_config+' --cflags --libs');
+		env['CPPUNIT_CXXFLAGS'] = SCons.Util.CLVar(
+			os.popen(cppunit_config+' --cflags 2>/dev/null').read().strip() );
+		env['CPPUNIT_LINKFLAGS'] = SCons.Util.CLVar(
+			os.popen(cppunit_config+' --libs 2>/dev/null').read().strip() );
