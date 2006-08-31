a421ea546daf149771bfc3be131ef734ab6cd19b
picca <picca@grisette.localdomain>
2006-08-31 18:54:30 +0200

    * add the ObjectReadOnly class. with this class you can only     set the name and the description in the constructor.

--- src/object.cpp
+++ src/object.cpp
@@ -4,70 +4,51 @@
 
 namespace hkl {
 
-    Object::Object(void)
-    : m_name("no name"),
-    m_description("no description")
-      {}
-
-    Object::Object(MyString const & name) throw (HKLException)
-    : m_description("no description")
-      {
-        set_name(name);
-      }
-
-    Object::Object(MyString const & name, MyString const & description) throw (HKLException)
-      {
-        set_name(name);
-        set_description(description);
-      }
-
-    Object::Object(Object const & object) :
-      m_name(object.m_name),
-      m_description(object.m_description)
-    {}
-
-    Object::~Object(void)
-      {}
-
-    bool
-    Object::operator == (Object const & object) const
-      {
-        return m_name == object.m_name
-        && m_description == object.m_description;
-      }
+    BaseObject::BaseObject(MyString const & name, MyString const & description)  throw (HKLException)
+    {
+      _set_name(name);
+      _set_description(description);
+    }
 
     void
-    Object::set_name(MyString const & name) throw (HKLException)
+    BaseObject::_set_name(MyString const & name) throw (HKLException)
       {
         //verification that the name is a valid name.
-        if (name.size() == 0)
+        if (name.size())
+            m_name = name;
+        else
           {
             ostringstream reason;
             reason << "Can not set the " << typeid(*this).name() << " name with an empty name";
             HKLEXCEPTION(reason.str(),
                          "Please set with a non empty name");
           }
-        else
-            m_name = name;
       }
 
     void
-    Object::set_description(MyString const & description) throw (HKLException)
+    BaseObject::_set_description(MyString const & description) throw (HKLException)
       {
-        //verification that the name is a valid name.
-        if (description.size() == 0)
+        //verification that the description is a valid description.
+        if (description.size())
+            m_description = description;
+        else
           {
             ostringstream reason;
             reason << "Can not set the " << typeid(*this).name() << " description with an empty description";
             HKLEXCEPTION(reason.str(),
                          "Please set with a non empty description");
           }
-        else
-            m_description = description;
+      }
+
+    bool
+    BaseObject::operator == (BaseObject const & object) const
+      {
+        return m_name == object.m_name
+        && m_description == object.m_description;
       }
 
     ostream & 
-    Object::printToStream(ostream & flux) const
+    BaseObject::printToStream(ostream & flux) const
       { 
         flux
         << " Name: " << m_name << endl
@@ -77,7 +58,7 @@ namespace hkl {
       }
 
     ostream &
-    Object::toStream(ostream & flux) const throw (HKLException)
+    BaseObject::toStream(ostream & flux) const
       {
         m_name.toStream(flux);
         m_description.toStream(flux);
@@ -86,7 +67,7 @@ namespace hkl {
       }
 
     istream &
-    Object::fromStream(istream & flux)
+    BaseObject::fromStream(istream & flux)
       {
         m_name.fromStream(flux);
         m_description.fromStream(flux);
@@ -94,10 +75,20 @@ namespace hkl {
         return flux;
       }
 
-} // namespace hkl
+    ObjectReadOnly::ObjectReadOnly(MyString const & name, MyString const & description) throw (HKLException) :
+      BaseObject(name, description)
+    {}
 
-ostream &
-operator << (ostream & flux, hkl::Object const & object)
-{
-    return object.printToStream(flux);
-}
+    Object::Object(void)
+    : ObjectReadOnly("no description", "no description")
+      {}
+
+    Object::Object(MyString const & name) throw (HKLException)
+    : ObjectReadOnly(name, "no description")
+      {}
+
+    Object::Object(MyString const & name, MyString const & description) throw (HKLException) :
+      ObjectReadOnly(name, description)
+    {}
+
+} // namespace hkl
