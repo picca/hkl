0c93e56099932a3da1bc43b87e24c74247ee0d46
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2008-08-28 08:57:52 +0200

    * add a degenerated case in the K4CV pseudoAxesEngine.

--- test/hkl-test-pseudoaxis-K4CV.c
+++ test/hkl-test-pseudoaxis-K4CV.c
@@ -119,13 +119,6 @@ HKL_TEST_SUITE_FUNC(set)
 			*K = k = (double)rand() / RAND_MAX * 2 - 1.;
 			*L = l = (double)rand() / RAND_MAX * 2 - 1.;
 
-			/* studdy this case */
-			/*
-			*H = h = 0;
-			*K = k = 1;
-			*L = l = 0;
-			*/
-
 			// pseudo -> geometry
 			res = hkl_pseudoAxisEngine_to_geometry(engine);
 			//hkl_pseudoAxisEngine_fprintf(engine, stdout);
@@ -155,10 +148,68 @@ HKL_TEST_SUITE_FUNC(set)
 	return HKL_TEST_PASS;
 }
 
+HKL_TEST_SUITE_FUNC(degenerated)
+{
+	HklPseudoAxisEngine *engine = NULL;
+	HklGeometry *geom;
+	HklDetector det = {1};
+	HklSample *sample;
+	size_t i, f_idx;
+	double *H, *K, *L;
+
+	geom = hkl_geometry_factory_new(HKL_GEOMETRY_KAPPA4C_VERTICAL, 50 * HKL_DEGTORAD);
+	sample = hkl_sample_new("test", HKL_SAMPLE_MONOCRYSTAL);
+
+	engine = hkl_pseudoAxisEngine_new_K4CV_HKL();
+
+	H = &engine->pseudoAxes[0].config.value;
+	K = &engine->pseudoAxes[1].config.value;
+	L = &engine->pseudoAxes[2].config.value;
+
+	for(f_idx=0; f_idx<engine->config.functions_len; ++f_idx) {
+		hkl_pseudoAxisEngine_set(engine, f_idx, geom, &det, sample);
+		if (f_idx>0)
+			engine->function->parameters[0].value = 1.;
+
+		double h, k, l;
+		int res;
+
+		/* studdy this degenerated case */
+		*H = h = 0;
+		*K = k = 1;
+		*L = l = 0;
+
+		// pseudo -> geometry
+		res = hkl_pseudoAxisEngine_to_geometry(engine);
+		//hkl_pseudoAxisEngine_fprintf(engine, stdout);
+
+		// geometry -> pseudo
+		if (res) {
+			for(i=0; i<engine->geometries_len; ++i) {
+				*H = *K = *L = 0;
+
+				hkl_geometry_init_geometry(engine->geometry, engine->geometries[i]);
+				hkl_pseudoAxisEngine_to_pseudoAxes(engine);
+
+				HKL_ASSERT_DOUBLES_EQUAL(h, *H, HKL_EPSILON);
+				HKL_ASSERT_DOUBLES_EQUAL(k, *K, HKL_EPSILON);
+				HKL_ASSERT_DOUBLES_EQUAL(l, *L, HKL_EPSILON);
+			}
+		}
+	}
+
+	hkl_pseudoAxisEngine_free(engine);
+	hkl_sample_free(sample);
+	hkl_geometry_free(geom);
+
+	return HKL_TEST_PASS;
+}
+
 HKL_TEST_SUITE_BEGIN
 
 HKL_TEST( new );
 //HKL_TEST( update );
 HKL_TEST( set );
+HKL_TEST( degenerated );
 
 HKL_TEST_SUITE_END
