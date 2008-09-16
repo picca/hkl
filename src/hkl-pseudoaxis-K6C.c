34c5514074b764b0d97a955ed58862a174043d36
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2008-09-16 08:56:42 +0200

    * add the constant omega vertical mode to the K6C diffractometer.

--- src/hkl-pseudoaxis-K6C.c
+++ src/hkl-pseudoaxis-K6C.c
@@ -123,6 +123,56 @@ static int bissector_v_f2(const gsl_vector *x, void *params, gsl_vector *f)
 	return  GSL_SUCCESS;
 }
 
+static int constant_omega_v_f1(const gsl_vector *x, void *params, gsl_vector *f)
+{
+	double komega, kappa, omega;
+	size_t i;
+	HklPseudoAxisEngine *engine = params;
+	double p0 = engine->getset->parameters[0].value;
+	double const *x_data = x->data;
+	double *f_data = f->data;
+
+	for(i=0; i<x->size;++i)
+		if (gsl_isnan(x_data[i]))
+			return GSL_ENOMEM;
+
+	RUBh_minus_Q(x_data, params, f_data);
+
+	komega = x_data[0];
+	kappa = x_data[1];
+
+	omega = komega + atan(tan(kappa/2.)*cos(50 * HKL_DEGTORAD)) - M_PI_2;
+
+	f_data[3] = p0 - omega;
+
+	return  GSL_SUCCESS;
+}
+
+static int constant_omega_v_f2(const gsl_vector *x, void *params, gsl_vector *f)
+{
+	double komega, kappa, omega;
+	size_t i;
+	HklPseudoAxisEngine *engine = params;
+	double p0 = engine->getset->parameters[0].value;
+	double const *x_data = x->data;
+	double *f_data = f->data;
+
+	for(i=0; i<x->size;++i)
+		if (gsl_isnan(x_data[i]))
+			return GSL_ENOMEM;
+
+	RUBh_minus_Q(x_data, params, f_data);
+
+	komega = x_data[0];
+	kappa = x_data[1];
+
+	omega = komega + atan(tan(kappa/2.)*cos(50 * HKL_DEGTORAD)) + M_PI_2;
+
+	f_data[3] = p0 - omega;
+
+	return  GSL_SUCCESS;
+}
+
 /*********************/
 /* Getter and Setter */
 /*********************/
@@ -157,6 +207,21 @@ static int hkl_pseudo_axis_engine_setter_func_bissector_v(HklPseudoAxisEngine *e
 	return res;
 }
 
+static int hkl_pseudo_axis_engine_setter_func_constant_omega_v(HklPseudoAxisEngine *engine,
+		HklGeometry *geometry, HklDetector *detector,
+		HklSample *sample)
+{
+	int res = 0;
+
+	hkl_pseudoAxeEngine_prepare_internal(engine, geometry, detector,
+			sample);
+
+	res |= hkl_pseudoAxeEngine_solve_function(engine, constant_omega_v_f1);
+	res |= hkl_pseudoAxeEngine_solve_function(engine, constant_omega_v_f2);
+
+	return res;
+}
+
 /************************/
 /* K6CV PseudoAxeEngine */
 /************************/
@@ -165,24 +230,35 @@ HklPseudoAxisEngine *hkl_pseudoAxisEngine_new_K6C_HKL(void)
 {
 	HklPseudoAxisEngine *self;
 	HklPseudoAxisEngineGetSet *getset;
+	HklParameter parameter = {NULL, {-M_PI, M_PI}, 0., 0};
 
 	self = hkl_pseudoAxisEngine_new("hkl", 3, "h", "k", "l");
 
-	/* hkl get/set bissector horizontal */
+	/* bissector_horizontal */
 	getset = hkl_pseudo_axis_engine_get_set_new(
-			"bissector horizontal",
+			"bissector_horizontal",
 			hkl_pseudo_axis_engine_getter_func_hkl,
 			hkl_pseudo_axis_engine_setter_func_bissector_h,
 			0,
 			5, "mu", "komega", "kappa", "kphi", "gamma");
 	hkl_pseudoAxisEngine_add_get_set(self, getset);
 
-	/* hkl get/set bissector vertical */
+	/* bissector_vertical */
 	getset = hkl_pseudo_axis_engine_get_set_new(
-			"bissector vertical",
+			"bissector_vertical",
 			hkl_pseudo_axis_engine_getter_func_hkl,
 			hkl_pseudo_axis_engine_setter_func_bissector_v,
 			0,
+			4, "komega", "kappa", "kphi", "delta");
+	hkl_pseudoAxisEngine_add_get_set(self, getset);
+
+	/* constant_omega_vertical */
+	parameter.name = "omega";
+	getset = hkl_pseudo_axis_engine_get_set_new(
+			"constant_omega_vertical",
+			hkl_pseudo_axis_engine_getter_func_hkl,
+			hkl_pseudo_axis_engine_setter_func_constant_omega_v,
+			1, &parameter,
 			4, "komega", "kappa", "kphi", "delta");
 	hkl_pseudoAxisEngine_add_get_set(self, getset);
 
