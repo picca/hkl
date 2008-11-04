3249774a4b8da0a693454a35688d20ba9ad9fead
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2008-11-04 09:38:25 +0100

    * add a degenerated case for the E6CV pseudoAxes.

--- test/hkl-test-pseudoaxis-E6C.c
+++ test/hkl-test-pseudoaxis-E6C.c
@@ -96,9 +96,68 @@ HKL_TEST_SUITE_FUNC(getter)
 	return HKL_TEST_PASS;
 }
 
+HKL_TEST_SUITE_FUNC(degenerated)
+{
+	HklPseudoAxisEngine *engine = NULL;
+	HklGeometry *geom;
+	HklDetector det = {1};
+	HklSample *sample;
+	size_t i, f_idx;
+	double *H, *K, *L;
+
+	geom = hkl_geometry_factory_new(HKL_GEOMETRY_EULERIAN6C);
+	sample = hkl_sample_new("test", HKL_SAMPLE_MONOCRYSTAL);
+
+	engine = hkl_pseudoAxisEngine_new_E6C_HKL();
+
+	H = &engine->pseudoAxes[0].config.value;
+	K = &engine->pseudoAxes[1].config.value;
+	L = &engine->pseudoAxes[2].config.value;
+
+	for(f_idx=0; f_idx<engine->getsets_len; ++f_idx) {
+		hkl_pseudoAxisEngine_select_get_set(engine, f_idx);
+		if (engine->getset->parameters_len)
+			engine->getset->parameters[0].value = 0.;
+
+		double h, k, l;
+		int res;
+
+		/* studdy this degenerated case */
+		*H = h = 0;
+		*K = k = 0;
+		*L = l = 1;
+
+		// pseudo -> geometry
+		res = hkl_pseudoAxisEngine_setter(engine, geom, &det, sample);
+		//hkl_pseudoAxisEngine_fprintf(stdout, engine);
+
+		// geometry -> pseudo
+		if (res == HKL_SUCCESS) {
+			//hkl_pseudoAxisEngine_fprintf(stdout, engine);
+			for(i=0; i<engine->geometries_len; ++i) {
+				*H = *K = *L = 0;
+
+				hkl_geometry_init_geometry(engine->geometry, engine->geometries[i]);
+				hkl_pseudoAxisEngine_getter(engine, engine->geometry, &det, sample);
+
+				HKL_ASSERT_DOUBLES_EQUAL(h, *H, HKL_EPSILON);
+				HKL_ASSERT_DOUBLES_EQUAL(k, *K, HKL_EPSILON);
+				HKL_ASSERT_DOUBLES_EQUAL(l, *L, HKL_EPSILON);
+			}
+		}
+	}
+
+	hkl_pseudoAxisEngine_free(engine);
+	hkl_sample_free(sample);
+	hkl_geometry_free(geom);
+
+	return HKL_TEST_PASS;
+}
+
 HKL_TEST_SUITE_BEGIN
 
 HKL_TEST( new );
 HKL_TEST( getter );
+HKL_TEST( degenerated );
 
 HKL_TEST_SUITE_END
