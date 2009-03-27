4dbcdb93cb8a8ff5ce857133b83d89058802639f
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2009-03-27 17:12:08 +0100

    * sort the geometries after an hkl_pseudo_axis_engine_setter.

--- src/hkl-pseudoaxis.c
+++ src/hkl-pseudoaxis.c
@@ -378,6 +378,8 @@ int hkl_pseudo_axis_engine_setter(HklPseudoAxisEngine *self, HklGeometry *geomet
 	if (self->mode && self->mode->set)
 		res = self->mode->set(self, geometry, detector, sample);
 
+	hkl_geometry_list_sort(self->engines->geometries, geometry);
+
 	return res;
 }
 
