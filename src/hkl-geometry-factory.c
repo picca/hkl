a6e309cd9e016e8f2892aa90af071feac3fdca86
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2010-01-04 17:25:47 +0100

    * update the copyright year

--- src/hkl-geometry-factory.c
+++ src/hkl-geometry-factory.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with the hkl library.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2003-2009 Synchrotron SOLEIL
+ * Copyright (C) 2003-2010 Synchrotron SOLEIL
  *                         L'Orme des Merisiers Saint-Aubin
  *                         BP 48 91192 GIF-sur-YVETTE CEDEX
  *
