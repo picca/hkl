9cf926035e9cb53e546717e70bd3c047fdd25ce4
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2010-03-01 19:50:45 +0100

    * add a new compilation flag -Wmissing-declarations
    to avoid futur bugs like functions signature discripencies

--- hkl/hkl-macros.h
+++ hkl/hkl-macros.h
@@ -23,6 +23,7 @@
 #define __HKL_MACROS_H__
 
 #include <stdlib.h>
+#include <stdarg.h>
 
 /* Guard C code in headers, while including them from C++ */
 #ifdef __cplusplus
@@ -112,6 +113,10 @@ HKL_BEGIN_DECLS
 extern void die(const char *err, ...) NORETURN __attribute__((format (printf, 1, 2)));
 
 extern void warning(const char *err, ...);
+
+extern void set_die_routine(void (*routine)(const char *err, va_list params) NORETURN);
+
+extern void set_warning_routine(void (*routine)(const char *err, va_list params));
 
 extern void hkl_printbt(void);
 
