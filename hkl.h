be344c27456256f466bae55ebea866ea6eae0d9b
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2010-08-29 13:15:31 +0200

    * typo

--- hkl.h
+++ hkl.h
@@ -1,7 +1,7 @@
 #include <hkl/hkl-error.h>
 #include <hkl/hkl-detector-factory.h>
 #include <hkl/hkl-pseudoaxis-factory.h>
-#include <hkl/hkl-pseudoaxis-e4cv.h>
+#include <hkl/hkl-pseudoaxis-e4c.h>
 #include <hkl/hkl-pseudoaxis-e6c.h>
 #include <hkl/hkl-pseudoaxis-k4cv.h>
 #include <hkl/hkl-pseudoaxis-k6c.h>
