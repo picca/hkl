6ee47cc9820a93d27a31cce75f80ea95e9fb257c
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2012-08-21 11:33:16 +0200

    add a binding dedicated method to the HklPseudoAxisEngineList
    now it is possible to iterate on each engine like this
    
    for engine in engines.engines():
    	print engine

--- hkl/hkl-pseudoaxis.h
+++ hkl/hkl-pseudoaxis.h
@@ -153,6 +153,13 @@ extern int hkl_pseudo_axis_engine_list_get(HklPseudoAxisEngineList *self);
 
 extern void hkl_pseudo_axis_engine_list_fprintf(FILE *f,
 						const HklPseudoAxisEngineList *self);
+
+/*********************/
+/* binding dedicated */
+/*********************/
+
+HklPseudoAxisEngine** hkl_pseudo_axis_engine_list_engines(HklPseudoAxisEngineList *self, uint *len);
+
 HKL_END_DECLS
 
 #endif /* __HKL_PSEUDOAXIS_H__ */
--- test/bindings/python.py
+++ test/bindings/python.py
@@ -29,30 +29,77 @@ from gi.repository import Hkl
 
 class TestAPI(unittest.TestCase):
 
-    def test_api(self):
+    def test_detector_api(self):
+        """
+        enforce the detector API
+        """
+
+        # create an 0D HklDetector
+        detector = Hkl.Detector().factory_new(Hkl.DetectorType(0))
+
+        # mount the detector on the 2nd holder of the geometry
+        # yes numbering follow the C convention !
+        detector.idx = 1
+        self.assertTrue(detector.idx == 1)
+
+    def test_geometry_api(self):
+        """
+        enforce the geometry API
+        """
+
+        # get the config for a given geometry and create the
+        # corresponding HklGeometry
+        config = Hkl.geometry_factory_get_config_from_type(
+            Hkl.GeometryType.KAPPA6C)
+        geometry = Hkl.Geometry.factory_newv(config, [50. * math.pi / 180.])
+
+        # set the geometry axes values
+        values_w = [0, 30, 0, 0, 0, 60]
+        geometry.set_axes_values_unit(values_w)
+        values_r = geometry.get_axes_values_unit()
+        # check that the read and write values of the geometry are
+        # almost equals
+        for r, w in zip(values_w, values_r):
+            self.assertAlmostEqual(r, w)
+
+    def test_sample_api(self):
+        """
+        enforce the HklSample API
+        """
+
+        # create a sample
+        sample = Hkl.Sample.new("toto", Hkl.SampleType.MONOCRYSTAL)
+        self.assertTrue(sample.name == "toto")
+
+        #set the lattice parameters
+        sample.set_lattice(1.54, 1.54, 1.54, 90., 90., 90.)
+
+    def test_pseudoaxis_api(self):
+        """
+        enforce the HklPseudoAxisEngine API
+        """
+
         detector = Hkl.Detector().factory_new(Hkl.DetectorType(0))
         detector.idx = 1
 
         config = Hkl.geometry_factory_get_config_from_type(
             Hkl.GeometryType.KAPPA6C)
-
         geometry = Hkl.Geometry.factory_newv(config, [50. * math.pi / 180.])
+        values_w = [0, 30, 0, 0, 0, 60]
+        geometry.set_axes_values_unit(values_w)
 
         sample = Hkl.Sample.new("toto", Hkl.SampleType.MONOCRYSTAL)
         sample.set_lattice(1.54, 1.54, 1.54, 90., 90., 90.)
 
+        # compute all the pseudo axes managed by all engines
         engines = Hkl.PseudoAxisEngineList.factory(config)
         engines.init(geometry, detector, sample)
-
-        # print the current geometry axes values
-        values = geometry.get_axes_values_unit()
-
-        # set the axes values
-        values = [0, 30, 0, 0, 0, 60]
-        geometry.set_axes_values_unit(values)
-
-        # compute all the pseudo axes managed by all engines
         engines.get()
+
+        for engine in engines.engines():
+            self.assertTrue(type(engine) is Hkl.PseudoAxisEngine)
+        #     for geometry in engine.geometries:
+        #         print geometry.get_axes_values_unit()
 
         self.assertTrue(True)
 
