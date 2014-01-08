2d991257432c87f3c827fc7e8ceb3d5047e74180
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2014-01-08 10:48:35 +0100

    now the mode is well reflected in the combobox

--- gui/hkl-gui-pseudoaxes.c
+++ gui/hkl-gui-pseudoaxes.c
@@ -51,131 +51,8 @@ struct _HklGuiEnginePrivate {
 	GtkListStore* store_mode_parameter;
 };
 
-static void hkl_gui_engine_set_property (GObject      *object,
-					 guint         param_id,
-					 const GValue *value,
-					 GParamSpec   *pspec);
-static void hkl_gui_engine_get_property (GObject     *object,
-					 guint        param_id,
-					 GValue      *value,
-					 GParamSpec  *pspec);
-
-static void hkl_gui_engine_finalize (GObject* obj);
-
-static void hkl_gui_engine_update_pseudo_axis (HklGuiEngine* self);
-
-static void hkl_gui_engine_update_mode (HklGuiEngine* self);
-
-static void hkl_gui_engine_update_mode_parameters (HklGuiEngine* self);
-
-static void hkl_gui_engine_on_combobox1_changed (GtkComboBox* combobox, HklGuiEngine* self);
-
-static void hkl_gui_engine_on_button1_clicked (GtkButton* button, HklGuiEngine* self);
-
-static void hkl_gui_engine_on_button2_clicked (GtkButton* button, HklGuiEngine* self);
-
-static void hkl_gui_engine_on_cell_tree_view_pseudo_axis_value_edited (GtkCellRendererText* renderer,
-								       const gchar* path,
-								       const gchar* new_text,
-								       HklGuiEngine* self);
 
 G_DEFINE_TYPE (HklGuiEngine, hkl_gui_engine, G_TYPE_OBJECT);
-
-static void
-hkl_gui_engine_class_init (HklGuiEngineClass * class)
-{
-	GObjectClass *gobject_class;
-	
-	gobject_class = (GObjectClass *) class;
-
-	gobject_class->finalize = hkl_gui_engine_finalize;
-
-	gobject_class->set_property = hkl_gui_engine_set_property;
-	gobject_class->get_property = hkl_gui_engine_get_property;
-
-	g_object_class_install_property (gobject_class,
-					 PROP_ENGINE,
-					 g_param_spec_pointer ("engine",
-							       "Engine",
-							       "The Hkl Engine used underneath",
-							       G_PARAM_READABLE |
-							       G_PARAM_WRITABLE));
-
-	g_signal_new ("changed",
-		      HKL_GUI_TYPE_ENGINE,
-		      G_SIGNAL_RUN_LAST,
-		      0, /* class offset */
-		      NULL, /* accumulator */
-		      NULL, /* accu_data */
-		      g_cclosure_marshal_VOID__VOID,
-		      G_TYPE_NONE, /* return_type */
-		      0);
-
-	g_type_class_add_private (class, sizeof (HklGuiEnginePrivate));
-}
-
-static void _connect_renderer(gpointer data, gpointer user_data)
-{
-	GtkCellRenderer *renderer = GTK_CELL_RENDERER(data);
-	HklGuiEngine *self = HKL_GUI_ENGINE(user_data);
-	
-	g_signal_connect_object (renderer,
-				 "edited",
-				 (GCallback) hkl_gui_engine_on_cell_tree_view_pseudo_axis_value_edited,
-				 self, 0);
-}
-
-static void hkl_gui_engine_init (HklGuiEngine * self)
-{
-	HklGuiEnginePrivate *priv;
-	GtkBuilder *builder;
-	GtkTreeViewColumn* col;
-	GList* cells;
-
-	self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-						  HKL_GUI_TYPE_ENGINE,
-						  HklGuiEnginePrivate);
-	priv = self->priv;
-
-	priv->engine = NULL;
-	priv->builder = builder = gtk_builder_new ();
-
-	gtk_builder_add_from_file (builder, "pseudo.ui", NULL);
-
-	priv->frame1 = GTK_FRAME(gtk_builder_get_object(builder, "frame1"));
-	priv->label2 = GTK_LABEL(gtk_builder_get_object(builder, "label2"));
-	priv->combobox1 = GTK_COMBO_BOX(gtk_builder_get_object(builder, "combobox1"));
-	priv->expander1 = GTK_EXPANDER(gtk_builder_get_object(builder, "expander1"));
-	priv->treeview1 = GTK_TREE_VIEW(gtk_builder_get_object(builder, "treeview1"));
-	priv->button1 = GTK_BUTTON(gtk_builder_get_object(builder, "button1"));
-	priv->button2 = GTK_BUTTON(gtk_builder_get_object(builder, "button2"));
-
-	priv->store_mode = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore1"));
-	priv->store_pseudo = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore2"));
-	priv->store_mode_parameter = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore3"));
-
-	gtk_builder_connect_signals (builder, self);
-
-	g_signal_connect_object (self->priv->combobox1,
-				 "changed",
-				 (GCallback) hkl_gui_engine_on_combobox1_changed,
-				 self, 0);
-
-	g_signal_connect_object (self->priv->button1,
-				 "clicked",
-				 (GCallback) hkl_gui_engine_on_button1_clicked,
-				 self, 0);
-
-	g_signal_connect_object (self->priv->button2,
-				 "clicked",
-				 (GCallback) hkl_gui_engine_on_button2_clicked,
-				 self, 0);
-
-	col = gtk_tree_view_get_column (self->priv->treeview1, 1);
-	cells = gtk_cell_layout_get_cells(GTK_CELL_LAYOUT(col));
-	g_list_foreach(cells, _connect_renderer, self);
-	g_list_free(cells);
-}
 
 static void hkl_gui_engine_set_property (GObject         *object,
 					 guint            prop_id,
@@ -184,12 +61,12 @@ static void hkl_gui_engine_set_property (GObject         *object,
 {
 	HklGuiEngine *self = HKL_GUI_ENGINE (object);
 	HklGuiEnginePrivate *priv = self->priv;
-	
+
 	switch (prop_id) {
 	case PROP_ENGINE:
 		hkl_gui_engine_set_engine(self, g_value_get_pointer (value));
 		break;
-	default:      
+	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 		break;
 	}
@@ -233,29 +110,6 @@ HklGuiEngine* hkl_gui_engine_new (HklEngine* engine)
 }
 
 
-void hkl_gui_engine_set_engine (HklGuiEngine *self,
-				HklEngine *engine)
-{
-	HklGuiEnginePrivate *priv;
-
-	g_return_if_fail (self != NULL);
-	g_return_if_fail (engine != NULL);
-	
-	priv = self->priv;
-
-	priv->engine = engine;
-
-	gtk_label_set_label (priv->label2,
-			     hkl_engine_name(priv->engine));
-
-	hkl_gui_engine_update_pseudo_axis (self);
-	hkl_gui_engine_update_mode (self);
-	hkl_gui_engine_update_mode_parameters (self);
-
-	hkl_gui_engine_update(self);
-}
-
-
 HklEngine* hkl_gui_engine_get_engine (HklGuiEngine *self)
 {
 	HklGuiEnginePrivate *priv;
@@ -275,30 +129,19 @@ GtkFrame *hkl_gui_engine_get_frame(HklGuiEngine *self)
 	return priv->frame1;
 }
 
-void hkl_gui_engine_update (HklGuiEngine* self)
+
+static void hkl_gui_engine_update_pseudo_axis (HklGuiEngine* self)
 {
-	GtkTreeViewColumn* col;
-	GList* cells;
-
-	g_return_if_fail (self != NULL);
-
-	hkl_gui_engine_update_pseudo_axis (self);
-
-	col = gtk_tree_view_get_column(self->priv->treeview1, 1);
-	cells = gtk_cell_layout_get_cells(GTK_CELL_LAYOUT(col));
-	g_object_set (G_OBJECT(cells->data), "background", NULL, NULL);
-	g_list_free(cells);
-}
-
-
-static void hkl_gui_engine_update_pseudo_axis (HklGuiEngine* self) {
+	HklGuiEnginePrivate *priv;
 	GtkTreeIter iter = {0};
 	darray_parameter *parameters;
 	HklParameter **parameter;
 
 	g_return_if_fail (self != NULL);
 
-	gtk_list_store_clear (self->priv->store_pseudo);
+	priv = self->priv;
+
+	gtk_list_store_clear (priv->store_pseudo);
 	parameters = hkl_engine_pseudo_axes (self->priv->engine);
 	darray_foreach(parameter, *parameters){
 		gtk_list_store_append (self->priv->store_pseudo,
@@ -313,23 +156,33 @@ static void hkl_gui_engine_update_pseudo_axis (HklGuiEngine* self) {
 }
 
 
-static void hkl_gui_engine_update_mode (HklGuiEngine* self) {
+static void hkl_gui_engine_update_mode (HklGuiEngine* self)
+{
+	HklGuiEnginePrivate *priv;
 	GtkTreeIter iter = {0};
+	GtkTreeIter current = {0};
 	darray_mode *modes;
 	HklMode **mode;
 
 	g_return_if_fail (self != NULL);
 
-	modes = hkl_engine_modes(self->priv->engine);
-	gtk_list_store_clear (self->priv->store_mode);
+	priv = self->priv;
+
+	modes = hkl_engine_modes(priv->engine);
+	gtk_list_store_clear (priv->store_mode);
 	darray_foreach(mode, *modes){
-		gtk_list_store_append (self->priv->store_mode,
+		gtk_list_store_append (priv->store_mode,
 				       &iter);
-		gtk_list_store_set (self->priv->store_mode,
+		gtk_list_store_set (priv->store_mode,
 				    &iter,
 				    MODE_COL_NAME, hkl_mode_name(*mode),
 				    -1);
+		if(*mode == hkl_engine_mode(priv->engine))
+			current = iter;
 	}
+
+	/* now set the active row with the current mode */
+	gtk_combo_box_set_active_iter(priv->combobox1, &current);
 }
 
 
@@ -362,6 +215,45 @@ static void hkl_gui_engine_update_mode_parameters (HklGuiEngine* self) {
 			gtk_widget_hide (GTK_WIDGET (self->priv->expander1));
 	}else
 		gtk_widget_hide (GTK_WIDGET(self->priv->expander1));
+}
+
+
+void hkl_gui_engine_update (HklGuiEngine* self)
+{
+	GtkTreeViewColumn* col;
+	GList* cells;
+
+	g_return_if_fail (self != NULL);
+
+	hkl_gui_engine_update_pseudo_axis (self);
+
+	col = gtk_tree_view_get_column(self->priv->treeview1, 1);
+	cells = gtk_cell_layout_get_cells(GTK_CELL_LAYOUT(col));
+	g_object_set (G_OBJECT(cells->data), "background", NULL, NULL);
+	g_list_free(cells);
+}
+
+
+void hkl_gui_engine_set_engine (HklGuiEngine *self,
+				HklEngine *engine)
+{
+	HklGuiEnginePrivate *priv;
+
+	g_return_if_fail (self != NULL);
+	g_return_if_fail (engine != NULL);
+
+	priv = self->priv;
+
+	priv->engine = engine;
+
+	gtk_label_set_label (priv->label2,
+			     hkl_engine_name(priv->engine));
+
+	hkl_gui_engine_update_pseudo_axis (self);
+	hkl_gui_engine_update_mode (self);
+	hkl_gui_engine_update_mode_parameters (self);
+
+	hkl_gui_engine_update(self);
 }
 
 /*************/
@@ -446,4 +338,101 @@ static void hkl_gui_engine_on_cell_tree_view_pseudo_axis_value_edited (GtkCellRe
 					    -1);
 		}
 	}
+}
+
+static void hkl_gui_engine_class_init (HklGuiEngineClass * class)
+{
+	GObjectClass *gobject_class;
+
+	gobject_class = (GObjectClass *) class;
+
+	gobject_class->finalize = hkl_gui_engine_finalize;
+
+	gobject_class->set_property = hkl_gui_engine_set_property;
+	gobject_class->get_property = hkl_gui_engine_get_property;
+
+	g_object_class_install_property (gobject_class,
+					 PROP_ENGINE,
+					 g_param_spec_pointer ("engine",
+							       "Engine",
+							       "The Hkl Engine used underneath",
+							       G_PARAM_READABLE |
+							       G_PARAM_WRITABLE));
+
+	g_signal_new ("changed",
+		      HKL_GUI_TYPE_ENGINE,
+		      G_SIGNAL_RUN_LAST,
+		      0, /* class offset */
+		      NULL, /* accumulator */
+		      NULL, /* accu_data */
+		      g_cclosure_marshal_VOID__VOID,
+		      G_TYPE_NONE, /* return_type */
+		      0);
+
+	g_type_class_add_private (class, sizeof (HklGuiEnginePrivate));
+}
+
+
+static void _connect_renderer(gpointer data, gpointer user_data)
+{
+	GtkCellRenderer *renderer = GTK_CELL_RENDERER(data);
+	HklGuiEngine *self = HKL_GUI_ENGINE(user_data);
+
+	g_signal_connect_object (renderer,
+				 "edited",
+				 (GCallback) hkl_gui_engine_on_cell_tree_view_pseudo_axis_value_edited,
+				 self, 0);
+}
+
+
+static void hkl_gui_engine_init (HklGuiEngine * self)
+{
+	HklGuiEnginePrivate *priv;
+	GtkBuilder *builder;
+	GtkTreeViewColumn* col;
+	GList* cells;
+
+	self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+						  HKL_GUI_TYPE_ENGINE,
+						  HklGuiEnginePrivate);
+	priv = self->priv;
+
+	priv->engine = NULL;
+	priv->builder = builder = gtk_builder_new ();
+
+	gtk_builder_add_from_file (builder, "pseudo.ui", NULL);
+
+	priv->frame1 = GTK_FRAME(gtk_builder_get_object(builder, "frame1"));
+	priv->label2 = GTK_LABEL(gtk_builder_get_object(builder, "label2"));
+	priv->combobox1 = GTK_COMBO_BOX(gtk_builder_get_object(builder, "combobox1"));
+	priv->expander1 = GTK_EXPANDER(gtk_builder_get_object(builder, "expander1"));
+	priv->treeview1 = GTK_TREE_VIEW(gtk_builder_get_object(builder, "treeview1"));
+	priv->button1 = GTK_BUTTON(gtk_builder_get_object(builder, "button1"));
+	priv->button2 = GTK_BUTTON(gtk_builder_get_object(builder, "button2"));
+
+	priv->store_mode = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore1"));
+	priv->store_pseudo = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore2"));
+	priv->store_mode_parameter = GTK_LIST_STORE(gtk_builder_get_object (builder, "liststore3"));
+
+	gtk_builder_connect_signals (builder, self);
+
+	g_signal_connect_object (self->priv->combobox1,
+				 "changed",
+				 (GCallback) hkl_gui_engine_on_combobox1_changed,
+				 self, 0);
+
+	g_signal_connect_object (self->priv->button1,
+				 "clicked",
+				 (GCallback) hkl_gui_engine_on_button1_clicked,
+				 self, 0);
+
+	g_signal_connect_object (self->priv->button2,
+				 "clicked",
+				 (GCallback) hkl_gui_engine_on_button2_clicked,
+				 self, 0);
+
+	col = gtk_tree_view_get_column (self->priv->treeview1, 1);
+	cells = gtk_cell_layout_get_cells(GTK_CELL_LAYOUT(col));
+	g_list_foreach(cells, _connect_renderer, self);
+	g_list_free(cells);
 }
