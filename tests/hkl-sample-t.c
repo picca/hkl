96c7b0eb5ce747b56ac447a25bbedff5c4ff9459
Picca Frédéric-Emmanuel <picca@debian.org>
2014-08-09 10:32:44 +0200

    get rid of all warning generated by unuser-result in hkl-sample-t

--- tests/hkl-sample-t.c
+++ tests/hkl-sample-t.c
@@ -136,7 +136,7 @@ static void  set_UB(void )
 
 	sample = hkl_sample_new("test");
 
-	hkl_sample_UB_set(sample, UB, NULL);
+	ok(TRUE == hkl_sample_UB_set(sample, UB, NULL), __func__);
 	ok(TRUE == hkl_matrix_cmp(U,
 				      hkl_sample_U_get(sample)), __func__);
 	is_double(-90. * HKL_DEGTORAD,
@@ -176,15 +176,15 @@ static void compute_UB_busing_levy(void)
 
 	sample = hkl_sample_new("test");
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.), __func__);
 	r0 = hkl_sample_reflection_new(geometry, detector, 0, 0, 1, NULL);
 	hkl_sample_add_reflection(sample, r0);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., -90., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., -90., 60.), __func__);
 	r1 = hkl_sample_reflection_new(geometry, detector, -1, 0, 0, NULL);
 	hkl_sample_add_reflection(sample, r1);
 
-	hkl_sample_compute_UB_busing_levy(sample, r0, r1, NULL);
+	ok(TRUE == hkl_sample_compute_UB_busing_levy(sample, r0, r1, NULL), __func__);
 	ok(TRUE == hkl_matrix_cmp(m_I, hkl_sample_U_get(sample)), __func__);
 	is_double(0.,
 		  hkl_parameter_value_get(hkl_sample_ux_get(sample), HKL_UNIT_DEFAULT),
@@ -196,15 +196,15 @@ static void compute_UB_busing_levy(void)
 		  hkl_parameter_value_get(hkl_sample_uz_get(sample), HKL_UNIT_DEFAULT),
 		  HKL_EPSILON, __func__);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.), __func__);
 	r2 = hkl_sample_reflection_new(geometry, detector, 1, 0, 0, NULL);
 	hkl_sample_add_reflection(sample, r2);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 180., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 180., 60.), __func__);
 	r3 = hkl_sample_reflection_new(geometry, detector, 0, 1, 0, NULL);
 	hkl_sample_add_reflection(sample, r3);
 
-	hkl_sample_compute_UB_busing_levy(sample, r2, r3, NULL);
+	ok(TRUE == hkl_sample_compute_UB_busing_levy(sample, r2, r3, NULL), __func__);
 	ok(TRUE == hkl_matrix_cmp(m_ref, hkl_sample_U_get(sample)), __func__);
 	is_double(-90. * HKL_DEGTORAD,
 		  hkl_parameter_value_get(hkl_sample_ux_get(sample), HKL_UNIT_DEFAULT),
@@ -251,27 +251,27 @@ static void affine(void)
 	hkl_sample_lattice_set(sample, lattice);
 	hkl_lattice_free(lattice);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 1, 0, 0, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 0, 1, 0, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 0, 0, 1, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, .625, .75, -.216506350946, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, .665975615037, .683012701892, .299950211252, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_sample_affine(sample, NULL);
+	ok(TRUE == hkl_sample_affine(sample, NULL), __func__);
 
 	hkl_lattice_get(hkl_sample_lattice_get(sample),
 			&a, &b, &c, &alpha, &beta, &gamma, HKL_UNIT_DEFAULT);
@@ -321,23 +321,23 @@ static void get_reflections_xxx_angle(void)
 	hkl_sample_lattice_set(sample, lattice);
 	hkl_lattice_free(lattice);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.), __func__);
 	r0 = hkl_sample_reflection_new(geometry, detector, 1, 0, 0, NULL);
 	hkl_sample_add_reflection(sample, r0);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.), __func__);
 	r1 = hkl_sample_reflection_new(geometry, detector, 0, 1, 0, NULL);
 	hkl_sample_add_reflection(sample, r1);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.), __func__);
 	r2 = hkl_sample_reflection_new(geometry, detector, 0, 0, 1, NULL);
 	hkl_sample_add_reflection(sample, r2);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.), __func__);
 	r3 = hkl_sample_reflection_new(geometry, detector, .625, .75, -.216506350946, NULL);
 	hkl_sample_add_reflection(sample, r3);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.), __func__);
 	r4 = hkl_sample_reflection_new(geometry, detector, .665975615037, .683012701892, .299950211252, NULL);
 	hkl_sample_add_reflection(sample, r4);
 
@@ -388,31 +388,31 @@ static void reflection_set_geometry(void)
 	hkl_sample_lattice_set(sample, lattice);
 	hkl_lattice_free(lattice);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 90., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 1, 0, 0, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 90., 0., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 0, 1, 0, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 30., 0., 0., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, 0, 0, 1, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 60., 60., 60., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, .625, .75, -.216506350946, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 46., 45., 45., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 46., 45., 45., 60.), __func__);
 	ref = hkl_sample_reflection_new(geometry, detector, .665975615037, .683012701892, .299950211252, NULL);
 	hkl_sample_add_reflection(sample, ref);
 
 	/* correct the last reflection so the sample affinement must be ok. */
-	hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.);
+	ok(TRUE == hkl_geometry_set_values_v(geometry, HKL_UNIT_USER, NULL, 45., 45., 45., 60.), __func__);
 	hkl_sample_reflection_geometry_set(ref, geometry);
 
-	hkl_sample_affine(sample, NULL);
+	ok(TRUE == hkl_sample_affine(sample, NULL), __func__);
 	hkl_lattice_get(hkl_sample_lattice_get(sample),
 			&a, &b, &c, &alpha, &beta, &gamma,
 			HKL_UNIT_DEFAULT);
@@ -442,7 +442,7 @@ static void reflection_set_geometry(void)
 
 int main(int argc, char** argv)
 {
-	plan(45);
+	plan(70);
 
 	new();
 	add_reflection();
