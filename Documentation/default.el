f1fe69052cc992f024945a929d2d602bdf55f8d8
picca <picca@synchrotron-soleil.fr>
2015-01-13 16:09:57 +0100

    add the default.el file to export the hkl.org.

---
+++ Documentation/default.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages
+   (quote
+    ((latex . t)
+     (python . t)
+     (emacs-lisp . t))))
+ '(org-confirm-babel-evaluate nil)
+ '(org-latex-listings (quote minted))
+ '(org-latex-minted-langs
+   (quote
+    ((emacs-lisp "common-lisp")
+     (cc "c++")
+     (cperl "perl")
+     (shell-script "bash")
+     (caml "ocaml")
+     (python "python"))))
+ '(org-latex-pdf-process
+   (quote
+    ("pdflatex -interaction nonstopmode --shell-escape -output-directory %o %f" "pdflatex -interaction nonstopmode --shell-escape -output-directory %o %f" "pdflatex -interaction nonstopmode --shell-escape -output-directory %o %f")))
+ '(org-export-with-sub-superscripts nil))
+ 
