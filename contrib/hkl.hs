b8808b8745f54de2ab524016350a1dc55dbbdbb4
Picca Frédéric-Emmanuel <picca@debian.org>
2015-03-10 20:34:43 +0100

    [contrib] add a commad line
    now it is possible to compute any hkl coordinates with
    
    hkl ca h k l

--- contrib/hkl.hs
+++ contrib/hkl.hs
@@ -26,6 +26,8 @@ import Numeric.Units.Dimensional.Prelude (_0, _1, _2, nano, meter, degree,
                                           Length, Angle, sin, cos, one, sqrt,
                                           Dimensionless, (*~~), (/~~))
 
+import Options.Applicative hiding ((<>))
+
 data Lattice = Cubic (Length Double) -- a = b = c, alpha = beta = gamma = 90
              | Tetragonal (Length Double) (Length Double) -- a = b != c, alpha = beta = gamma = 90
              | Orthorhombic (Length Double) (Length Double) (Length Double) -- a != b != c,  alpha = beta = gamma = 90
@@ -161,16 +163,46 @@ dispv = putStr . vecdisp (disps 2)
 disp :: Matrix Double -> IO ()
 disp = putStr . dispf 3
 
-main :: IO()
-main = do
-  print (solution /~~ degree)
-  dispv (computeHkl e4c solution lattice)
-  disp path
-       where
-         (sol, path) = computeAngles e4c angles lattice mode [0, 0, 1]
-         s = [30.0, 0.0, 0.0, 0.0, 10.0, 0.0]
-         d = [60.0]
-         angles = (s ++ d) *~~ degree
-         solution = fromMode mode sol angles
-         lattice = Cubic (1.54 *~ nano meter)
-         mode = ModeHklE4CConstantPhi
+-- command parsing
+data Command
+    = Ca Double Double Double -- ca command
+
+data Options
+    = Options Command
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+parseCa :: Parser Command
+parseCa = Ca
+    <$> argument auto (metavar "H")
+    <*> argument auto (metavar "K")
+    <*> argument auto (metavar "L")
+
+parseCommand :: Parser Command
+parseCommand = subparser $
+    command "ca"   (parseCa `withInfo` "compute angles for the given hkl")
+
+parseOptions :: Parser Options
+parseOptions = Options <$> parseCommand
+
+-- Actual program logic
+run :: Options -> IO ()
+run (Options cmd) =
+    case cmd of
+        Ca h k l-> do
+           print (solution /~~ degree)
+           dispv (computeHkl e4c solution lattice)
+           disp path
+               where
+                 (sol, path) = computeAngles e4c angles lattice mode [h, k, l]
+                 s = [30.0, 0.0, 0.0, 0.0, 10.0, 0.0]
+                 d = [60.0]
+                 angles = (s ++ d) *~~ degree
+                 solution = fromMode mode sol angles
+                 lattice = Cubic (1.54 *~ nano meter)
+                 mode = ModeHklE4CConstantPhi
+
+main :: IO ()
+main = run =<< execParser
+    (parseOptions `withInfo` "Interact with hkl API")
