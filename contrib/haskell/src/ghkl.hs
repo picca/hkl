18999cbd9d680fbfaeb8f3f296df828f468cd2d3
System User <picca@synchrotron-soleil.fr>
2016-04-12 14:23:48 +0200

    [contrib] sixs test

--- contrib/haskell/src/ghkl.hs
+++ contrib/haskell/src/ghkl.hs
@@ -4,28 +4,31 @@ import Control.Monad
 import Data.Map.Strict (lookup)
 import Data.Maybe (isNothing, fromJust)
 import Hkl
-import Numeric.LinearAlgebra (fromList)
+-- import Numeric.LinearAlgebra (fromList)
 import Numeric.Units.Dimensional.Prelude (nano, meter,
                                           (*~))
-import Pipes
-import qualified Pipes.Prelude as P
+-- import Pipes
+-- import qualified Pipes.Prelude as P
 import Prelude hiding (lookup)
 
 main' :: IO ()
 main' = do
   factories_ <- factories
-  let mfactory = lookup "E6C" factories_
+  let mfactory = lookup "ZAXIS" factories_
   if isNothing mfactory
   then
       return $ error $ "wrong diffractometer:" ++ show factories_
   else do
     let factory = fromJust mfactory
-    let sample = Sample "test" (Cubic (1.54 *~ nano meter))
-                   (Parameter "ux" 0.0 (Range (-180) 180))
-                   (Parameter "uy" 0.0 (Range (-180) 180))
-                   (Parameter "uz" 0.0 (Range (-180) 180))
+    let sample = Sample "test" (Orthorhombic
+                                (1.05394 *~ nano meter)
+                                (0.25560 *~ nano meter)
+                                (1.49050 *~ nano meter))
+                   (Parameter "ux" (-89.8821) (Range (-180) 180))
+                   (Parameter "uy" 0.1733 (Range (-180) 180))
+                   (Parameter "uz" (-84.0081) (Range (-180) 180))
 
-    let geometry = Geometry (Source (1.54 *~ nano meter)) [0, 30, 0, 0, 0, 60]
+    let geometry = Geometry (Source (0.0672929 *~ nano meter)) [0.1794, -160.0013, 21.1381, 0.5194]
     let detector = Detector DetectorType0D
 
     -- compute the pseudo axes values
@@ -33,23 +36,24 @@ main' = do
     -- print pseudoAxes
 
     -- solve a pseudo axis problem for the given engine
-    let engine = Engine "hkl" [ Parameter "h" 0.0 (Range (-1.0) 1.0)
-                              , Parameter "k" 0.0 (Range (-1.0) 1.0)
-                              , Parameter "l" 1.0 (Range (-1.0) 1.0)
+    let engine = Engine "hkl" [ Parameter "h" 4.0 (Range (-1.0) 1.0)
+                              , Parameter "k" 1.0 (Range (-1.0) 1.0)
+                              , Parameter "l" 0.3 (Range (-1.0) 1.0)
                               ]
-                 (Mode "bissector_vertical" [])
+                 (Mode "zaxis" [])
 
-    -- print =<< solve factory geometry detector sample engine
-    let from = fromList [0, 0, 1 :: Double]
-    let to = fromList [0, 1, 1 :: Double]
-    runEffect $ fromToPipe 20 from to
-              >-> P.print
-    -- solve a trajectory with Pipes
-    runEffect $ fromToPipe 1000 from to
-              >-> enginesTrajectoryPipe engine
-              >-> solveTrajPipe factory geometry detector sample
-              -- >-> P.print
-              >-> P.drain
+    print =<< solve factory geometry detector sample engine
+
+    -- let from = fromList [0, 0, 1 :: Double]
+    -- let to = fromList [0, 1, 1 :: Double]
+    -- runEffect $ fromToPipe 20 from to
+    --           >-> P.print
+    -- -- solve a trajectory with Pipes
+    -- runEffect $ fromToPipe 10000 from to
+    --           >-> enginesTrajectoryPipe engine
+    --           >-> solveTrajPipe factory geometry detector sample
+    --           >-> P.print
+    --           -- >-> P.drain
 
     return ()
 
