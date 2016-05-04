15a128980fa79c1735922b9fb25995aa31113ed4
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2016-05-04 16:39:28 +0200

    [contrib] simplification of the blender code

--- contrib/cirpad/blender_pyfai.py
+++ contrib/cirpad/blender_pyfai.py
@@ -7,9 +7,11 @@ import random
 import bmesh
 
 
+SCALE = 100
+
 # Definition du détecteur Cirpad
 
-class Cirpad(): # (Detector):
+class Cirpad(pyFAI.detectors.Detector): # (Detector):
     MAX_SHAPE = (11200, 120)
     IS_FLAT = False
     IS_CONTIGUOUS = False
@@ -79,10 +81,11 @@ class Cirpad(): # (Detector):
 
         n_corners = corners
         # Puis on calcule les coins pour les 19 modules restant
-        for i in range(19):
-            n_corners = self.passage(n_corners,self.rot)
-            corners = numpy.concatenate((corners,n_corners), axis = 0) # A voir la disposition finale souhaité
+        for i in range(1, 20):
+            n_corners = self.passage(n_corners, self.rot)
+            corners = numpy.concatenate((corners, n_corners), axis=0) # A voir la disposition finale souhaité
         return corners
+
     # Pas fait encore
     def calc_cartesian_positions(self,d1 = None, d2 = None, center = True, use_cython = True):
         if (d1 is None) or d2 is None:
@@ -177,6 +180,7 @@ def M(theta, u):
              u[1] * u[2] * one_minus_c + u[0] * s,
              c + u[2]**2 * one_minus_c]]
 
+
 def rotation(md,rot):
     shape = md.shape
     axe = numpy.array([[1,0,0],[0,1,0],[0,0,1]])    # A voir si ce n'est pas une entrée
@@ -187,21 +191,15 @@ def rotation(md,rot):
         nmd =  numpy.transpose(numpy.tensordot(P,numpy.transpose(md), axes=1))
     return(nmd)
 
-def translation(md,u):
+
+def translation(md, u):
     nmd = md + u
     return(nmd)
 
 # Change coordonée pour passer de celle de pyFAI à celle de l'affichage.
 
-def inter(array):
-    ar = numpy.zeros((3,))
-    ar[0] = array[0]
-    ar[2] = array[1]
-    ar[1] = array[2]
-    return(ar)
-
 def inter2(array):
-    ar = numpy.zeros(numpy.shape(array))
+    ar = numpy.empty(numpy.shape(array))
     ar[0] = array[2]
     ar[1] = array[1]
     ar[2] = array[0]
@@ -217,7 +215,7 @@ def print_corners(corners, name):
     contour_mesh .update()
     contour_objet = bpy.data.objects.new(name, contour_mesh )
     scene = bpy.context.scene
-    contour_objet .active_material = det
+    contour_objet.active_material = mat1
     scene.objects.link(contour_objet)
 
     bpy.context.scene.objects.active = bpy.context.scene.objects[name]
@@ -228,95 +226,87 @@ def print_corners(corners, name):
 
 
 # Materiau classique utilisé
-det = bpy.data.materials.new('Det')
-det.diffuse_color = (1,1,1)
+mat1 = bpy.data.materials.new('Mat1')
+mat1.diffuse_color = (1,1,1)
 
-cet = bpy.data.materials.new('Cet')
-cet.diffuse_color = (0,1,0)
+mat2 = bpy.data.materials.new('Mat2')
+mat2.diffuse_color = (0,1,0)
 
-# Affiche le contour des pixels pour tous les détecteur sauf Aarhus
-def print_detectors(detectors,names): # On affiche module par module
-    array = detectors.get_pixel_corners()
-    for i in range(0,detectors.MAX_SHAPE[0],detectors.MODULE_SIZE[0]):
-        for j in range(0,detectors.MAX_SHAPE[1],detectors.MODULE_SIZE[1]):
-            corners = extract(array,i,((i // detectors.MODULE_SIZE[0])+1) * detectors.MODULE_SIZE[0],j,((j // detectors.MODULE_SIZE[1])+1) * detectors.MODULE_SIZE[1])*100
-            shape = corners.shape
-            vertices = []
-            edges = [[2*i,2*i+1] for i in range(shape[0]+1+shape[1]+1)]
-            for l in range(shape[0]): # Traits horizontales
-                vertices = vertices + [inter(corners[l][0][0,:]), inter(corners[l][shape[1]-1][3,:])]
-            vertices = vertices + [inter(corners[shape[0]-1][0][1,:]), inter(corners[shape[0]-1][shape[1]-1][2,:])]
-
-            for k in range(shape[1]):
-                vertices.extend([inter(corners[0][k][0,:]),inter(corners[shape[0]-1][k][1,:])])
-            vertices = vertices + [inter(corners[0][shape[1]-1][3,:]), inter(corners[shape[0]-1][shape[1]-1][2,:])]
-
-            contour_mesh = bpy.data.meshes.new("contour_mesh")
-            contour_mesh .from_pydata(vertices, edges, [])
-            contour_mesh .update()
-            contour_objet = bpy.data.objects.new(names, contour_mesh )
-            scene = bpy.context.scene
-            contour_objet .active_material = det
-            scene.objects.link(contour_objet)
-
-    bpy.context.scene.objects.active = bpy.context.scene.objects[names]
-    bpy.ops.object.select_by_type(type = 'MESH')
-    bpy.ops.object.join()
-
-# Extrait une partie d'une matrice corners
-def extract(array_pixels_corners,a,b,c,d):
-    shape = numpy.shape(array_pixels_corners)
-    narray = numpy.zeros((b-a,d-c,shape[2],shape[3]))
-    for i in range(b-a):
-        for j in range(d-c):
-            narray[i][j] = array_pixels_corners[a+i][c+j]
-    return(narray)
 
 # Affiche points centres pixels
-def print_centers(centers,name):
+def print_centers(centers, name):
     shape = centers[0].shape
-    m = numpy.zeros((3,shape[0],shape[1]))
+    m = numpy.zeros((3, shape[0], shape[1]))
     for i in range(3):
         if centers[i] is not None:
-            m[i] = centers[i]*100
+            m[i] = centers[i] * SCALE
+
     m = inter2(m)
-    vertices = []
+
+    # Creation des vertices
     vertices = numpy.reshape(numpy.transpose(m), (shape [0] * shape[1], 3))
+
+    # Creation du mesh à partir des vertices
     contour_mesh = bpy.data.meshes.new("contour_mesh")
-    contour_mesh .from_pydata(vertices, [], [])
-    contour_mesh .update()
-    contour_objet = bpy.data.objects.new(name, contour_mesh )
+    contour_mesh.from_pydata(vertices, [], [])
+    contour_mesh.update()
+
+    # Creation d'un object avec ce mesh
+    contour_objet = bpy.data.objects.new(name, contour_mesh)
+
+    # si pas de materiaux, le menu n'apparait pas ???
+    contour_objet.active_material = mat2
+
+    # ajout de l'objet à la scène pour qu'il devienne visible
     scene = bpy.context.scene
-    contour_objet .active_material = cet
     scene.objects.link(contour_objet)
-
-
-
 
 
 # Affiche le detecteur avec ses couleurs, pas tourner dans le bon sens pour l'instant
-def print_pixel(corners,image):
+def print_pixel(corners, image):
+
     shape = corners.shape
-    # Creer le module
-    vertices = numpy.reshape(corners*100, (shape[0] * shape [1] * shape[2], 3))
-    faces = []
+
+    # Creer des vertices du detecteur
+    vertices = numpy.reshape(corners * SCALE, (shape[0] * shape [1] * shape[2], 3))
+
+    # creation de la list contenant les indices des vertices formant une face (un pixel)
     faces = [[4*i,4*i+1,4*i+2,4*i+3] for i in range(shape[0]*shape[1])]
+
+    # creation du mesh avec ces vertices et ces faces.
     contour_mesh = bpy.data.meshes.new("contour_mesh")
-    contour_mesh .from_pydata(vertices, [], faces)
-    contour_mesh .update()
-    contour_objet = bpy.data.objects.new('i', contour_mesh )
+    contour_mesh.from_pydata(vertices, [], faces)
+    contour_mesh.update()
+
+    # creation de l'object a partir du mesh
+    contour_objet = bpy.data.objects.new('detector', contour_mesh )
+
+    contour_objet.active_material = mat1
+
+    # ajout de l'object dans la scene.
     scene = bpy.context.scene
-    contour_objet .active_material = det
     scene.objects.link(contour_objet)
+
     # Ajoute les couleurs
+    # on cree un groupe de vertex par couleurs de la LUT
+    # ici la lut est simple 0-255 (sur le rouge)
     max = int(image.max()) + 1
     MyVertices = [[] for i in range(max)]
-    # Créer groupe par couleur
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            MyVertices[int(image[i,j])].extend([4*(i*shape[1]+j),4*(i*shape[1]+j)+1,4*(i*shape[1]+j)+2,4*(i*shape[1]+j)+3])
 
-    bpy.context.scene.objects.active = bpy.context.scene.objects['i']
+    # On partour toute l'image et pour chaque intensité, on rajoute les index des
+    # vertex de la list vertices formant une face dans le bon groupe de vertex.
+    # TODO improve speed :)
+    width = shape[1]
+
+    # TODO voir comment utiliser plutot les faces pour ces couleurs.
+    for i in range(shape[0]):
+        i_width = i * width
+        for j in range(shape[1]):
+            i_width_j = 4 * (i_width + j)
+            l = range(i_width_j, i_width_j + 4)
+            MyVertices[int(image[i,j])].extend(l)
+    bpy.context.scene.objects.active = bpy.context.scene.objects['detector']
+
     # Met les couleurs par groupe
     for k in range(max):
 
@@ -350,65 +340,19 @@ def print_pixel(corners,image):
 
 
 # Fonction servant a créé un materiau par couleur, dépend de l'intensité
-def MakeMaterial_1(k,max):
+def MakeMaterial_1(k, max):
     mat = bpy.data.materials.new("Mat2")
     mat.diffuse_shader = 'MINNAERT'
     mat.diffuse_color = (1, round(1-k/max,4), round(1-k/max,4))
     mat.darkness = 0.8
     return mat
 
-# Test en utilisant UV maping ne marche pas
-def print_pixel2(corners,image):
-    shape = corners.shape
-    vertices = numpy.reshape(corners*100, (shape[0] * shape [1] * shape[2], 3))
-    faces = []
-    faces = [[4*i,4*i+1,4*i+2,4*i+3] for i in range(shape[0]*shape[1])]
-    contour_mesh = bpy.data.meshes.new("contour_mesh")
-    contour_mesh .from_pydata(vertices, [], faces)
-    contour_mesh .update()
-    contour_objet = bpy.data.objects.new('i1', contour_mesh )
-    scene = bpy.context.scene
-    print(dir(contour_objet.data))
-    scene.objects.link(bpy.context.scene.objects['Cube'].data)
-    bpy.context.scene.objects.active = bpy.context.scene.objects['i1']
-    me = contour_objet.data
-#    me = ob.data
-    me.materials.append(det)
-    me.materials.append(cet)
-    # Assign materials to faces
-    for f in me.faces:
-        f.material_index = f.index % 2
-
-
-
-#obj = bpy.context.active_object
-#    me = obj.data
-#    bpy.ops.object.mode_set(mode = 'EDIT')
-#    bm = bmesh.from_edit_mesh(me)
-#    uv_layer = bm.loops.layers.uv.verify()
-#    bm.faces.layers.tex.verify() # currently blender needs both layers.
-    # adjust UVs
-#    for f in bm.faces:
-#        for l in f.loops:
-#            luv = l[uv_layer]
-#            if luv.select:
-#                # apply the location of the vertex as a UV
-#                luv.uv = l.vert.co.xy
-#    bmesh.update_edit_mesh(me)
-
 detector = pyFAI.detectors.ImXPadS70()
 corners = detector.get_pixel_corners()
 centers = detector.calc_cartesian_positions()
 
-shape = corners.shape
+shape = corners.shape[0:2]
 
-image = numpy.zeros((shape[0],shape[1]))
-
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        image[i,j] = random.randint(0, 50)
-
-#for i in range(shape[0]):
-#    image[i,50] = i
+image = numpy.random.randint(256, size=shape)
 
 print_pixel(corners,image)
