4224a8cee480dab082ba11a8efa22160b986f00a
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2016-07-06 13:48:48 +0200

    [contrib] wip martinetto

--- contrib/haskell/src/Hkl/Diffabs/Martinetto.hs
+++ contrib/haskell/src/Hkl/Diffabs/Martinetto.hs
@@ -1,21 +1,43 @@
-module Hkl.Diffabs.Martinetto
-       ( main_martinetto ) where
-
-import Control.Monad
-import Data.Attoparsec.Text
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
+module Hkl.Diffabs.Martinetto
+       ( main_diffabs
+       , main_martinetto ) where
+
+#if __GLASGOW_HASKELL__ < 710
+import Control.Applicative ((<$>), (<*>))
+#endif
+import Control.Exception (bracket)
+import Control.Monad (forM_)
+import Data.Attoparsec.Text
+import Data.ByteString.Char8 (pack)
 import Data.Either
 import Data.List (sort)
 import Data.Text (Text, intercalate, pack)
 import Data.Text.IO
+import Data.Vector.Storable (concat, head)
+import Hkl.H5 ( Dataset
+              , File
+              , check_ndims
+              , closeDataset
+              , get_position
+              , lenH5Dataspace
+              , openDataset
+              , withH5File )
 import Hkl.Types
 import Hkl.PyFAI
-import Numeric.Units.Dimensional.Prelude (meter, degree, (/~))
-import System.FilePath
+import Numeric.Units.Dimensional.Prelude (meter, degree, nano, (/~), (*~))
+import System.FilePath ((</>))
 import System.FilePath.Glob
 import System.IO (withFile, IOMode(WriteMode) )
 
-import Prelude hiding (lookup, readFile)
+import Prelude hiding (concat, head, lookup, print, readFile)
+
+import Pipes (Producer, lift, (>->), runEffect, yield)
+import Pipes.Prelude (print)
+
+
+-- {-# ANN module "HLint: ignore Use camelCase" #-}
 
 
 -- import Graphics.Rendering.Chart.Easy
@@ -32,21 +54,23 @@ import Prelude hiding (lookup, readFile)
 --       extract (PoniEntry _ _ (Length poni1) _ _ _ _ _ _) = poni1
 
 title :: Text
-title = intercalate (pack "\t") (map pack [ "# distance"
-                                          , "poni1"
-                                          , "poni2"
-                                          , "rot1"
-                                          , "rot2"
-                                          , "rot3" ])
+title = intercalate (Data.Text.pack "\t")
+        (map Data.Text.pack [ "# distance"
+                            , "poni1"
+                            , "poni2"
+                            , "rot1"
+                            , "rot2"
+                            , "rot3" ])
 
 toText :: PoniEntry -> Text
 toText (PoniEntry _ _ _ _ d p1 p2 rot1 rot2 rot3 _ _) =
-  intercalate (pack "\t") (map (pack . show) [ d /~ meter
-                                             , p1 /~ meter
-                                             , p2 /~ meter
-                                             , rot1 /~ degree
-                                             , rot2 /~ degree
-                                             , rot3 /~ degree])
+  intercalate (Data.Text.pack "\t")
+  (map (Data.Text.pack . show) [ d /~ meter
+                               , p1 /~ meter
+                               , p2 /~ meter
+                               , rot1 /~ degree
+                               , rot2 /~ degree
+                               , rot3 /~ degree])
 
 
 
@@ -74,8 +98,130 @@ main_martinetto = do
   let published = project </> "published-data"
   let calibration = published </> "calibration"
   let output = calibration </> "ponies.txt"
+  -- let nxs = project </> "XRD18keV_26.nxs"
   -- let filename = "/home/picca/tmp/reguer/rocha/merged.poni"
   -- let filename = "../cirpad/blender/test2.poni"
   filenames <- glob $ calibration </> "XRD18keV_26*.poni"
+  -- print $ sort filenames
+  -- print $ [0,3..39 :: Int] ++ [43 :: Int]
+  -- print nxs
   entries <- ponies filenames
   save output entries
+
+
+data DataFrame =
+  DataFrame { df_n :: Int
+            , df_geometry :: Geometry
+            } deriving (Show)
+
+class Frame t where
+  len :: t -> IO (Maybe Int)
+  row :: t -> Int -> IO DataFrame
+
+data DataFrameH5Path =
+  DataFrameH5Path { h5pImage :: DataItem
+                  , h5pMu :: DataItem
+                  , h5pKomega :: DataItem
+                  , h5pKappa :: DataItem
+                  , h5pKphi :: DataItem
+                  , h5pGamma :: DataItem
+                  , h5pDelta :: DataItem
+                  , h5pWavelength :: DataItem
+                  , h5pDiffractometerType :: DataItem
+                  } deriving (Show)
+
+data DataFrameH5 =
+  DataFrameH5 { h5image :: Dataset
+              , h5mu :: Dataset
+              , h5komega :: Dataset
+              , h5kappa :: Dataset
+              , h5kphi :: Dataset
+              , h5gamma :: Dataset
+              , h5delta :: Dataset
+              , h5wavelength :: Dataset
+              , h5dtype :: Dataset
+              }
+
+instance Frame DataFrameH5 where
+  len d =  lenH5Dataspace (h5delta d)
+  row d idx = do
+    mu <- get_position (h5mu d) idx
+    komega <- get_position (h5komega d) idx
+    kappa <- get_position (h5kappa d) idx
+    kphi <- get_position (h5kphi d) idx
+    gamma <- get_position (h5gamma d) idx
+    delta <- get_position (h5delta d) idx
+    wavelength <- get_position (h5wavelength d) 0
+    let source = Source (head wavelength *~ nano meter)
+    let positions = concat [mu, komega, kappa, kphi, gamma, delta]
+    return DataFrame { df_n = idx
+                     , df_geometry = Geometry source positions Nothing
+                     }
+
+withDataframeH5 :: File -> DataFrameH5Path -> (DataFrameH5 -> IO r) -> IO r
+withDataframeH5 h5file dfp = bracket (hkl_h5_open h5file dfp) hkl_h5_close
+
+hkl_h5_open :: File -> DataFrameH5Path -> IO DataFrameH5
+hkl_h5_open h5file dp = DataFrameH5
+                         <$> openDataset' h5file (h5pImage dp)
+                         <*> openDataset' h5file (h5pMu dp)
+                         <*> openDataset' h5file (h5pKomega dp)
+                         <*> openDataset' h5file (h5pKappa dp)
+                         <*> openDataset' h5file (h5pKphi dp)
+                         <*> openDataset' h5file (h5pGamma dp)
+                         <*> openDataset' h5file (h5pDelta dp)
+                         <*> openDataset' h5file (h5pWavelength dp)
+                         <*> openDataset' h5file (h5pDiffractometerType dp)
+  where
+    openDataset' :: File -> DataItem -> IO Dataset
+    openDataset' hid (DataItem name _) = openDataset hid (Data.ByteString.Char8.pack name) Nothing
+
+hkl_h5_is_valid :: DataFrameH5-> IO Bool
+hkl_h5_is_valid d = do
+  True <- check_ndims (h5mu d) 1
+  True <- check_ndims (h5komega d) 1
+  True <- check_ndims (h5kappa d) 1
+  True <- check_ndims (h5kphi d) 1
+  True <- check_ndims (h5gamma d) 1
+  True <- check_ndims (h5delta d) 1
+  return True
+
+hkl_h5_close :: DataFrameH5 -> IO ()
+hkl_h5_close df = do
+  closeDataset (h5image df)
+  closeDataset (h5mu df)
+  closeDataset (h5komega df)
+  closeDataset (h5kappa df)
+  closeDataset (h5kphi df)
+  closeDataset (h5gamma df)
+  closeDataset (h5delta df)
+  closeDataset (h5wavelength df)
+  closeDataset (h5dtype df)
+
+getDataFrame :: DataFrameH5 -> Producer DataFrame IO ()
+getDataFrame d = do
+  (Just n) <- lift $ len d
+  forM_ [0..n-1] (\i -> lift (row d i) >>= yield)
+
+main_diffabs :: IO ()
+main_diffabs = do
+  let root = "/tmp"
+  let filename = "XRD18keV_27.nxs"
+  let dataframe_h5p = DataFrameH5Path
+                      { h5pImage = DataItem "scan_27/scan_data/data_53" StrictDims
+                      , h5pMu = DataItem "scan_27/DIFFABS/d13-1-cx1__EX__DIF.1-MU__#1/raw_value" ExtendDims
+                      , h5pKomega = DataItem "scan_27/DIFFABS/d13-1-cx1__EX__DIF.1-KOMEGA__#1/raw_value" ExtendDims
+                      , h5pKappa = DataItem "scan_27/DIFFABS/d13-1-cx1__EX__DIF.1-KAPPA__#1/raw_value" ExtendDims
+                      , h5pKphi = DataItem "scan_27/DIFFABS/d13-1-cx1__EX__DIF.1-KPHI__#1/raw_value" ExtendDims
+                      , h5pGamma = DataItem "scan_27/DIFFABS/d13-1-cx1__EX__DIF.1-GAMMA__#1/raw_value" ExtendDims
+                      , h5pDelta = DataItem "scan_27/scan_data/trajectory_1_1" ExtendDims
+                      , h5pWavelength = DataItem "scan_27/DIFFABS/D13-1-C03__OP__MONO__#1/wavelength" StrictDims
+                      , h5pDiffractometerType = DataItem "scan_27/DIFFABS/I14-C-CX2__EX__DIFF-UHV__#1/type" StrictDims
+                      }
+
+  withH5File (root </> filename) $ \h5file ->
+    withDataframeH5 h5file dataframe_h5p $ \dataframe_h5 -> do
+      True <- hkl_h5_is_valid dataframe_h5
+
+      runEffect $ getDataFrame dataframe_h5
+        >-> print
