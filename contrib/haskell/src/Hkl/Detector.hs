cf1715ee90046452552ccf010ac71221a6ccc6dd
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2016-09-28 14:48:41 +0200

    [contrib][haskell] more simplification

--- contrib/haskell/src/Hkl/Detector.hs
+++ contrib/haskell/src/Hkl/Detector.hs
@@ -44,16 +44,16 @@ coordinates Xpad32 (NptPoint x y) = fromList [g (round y), f (round x), 0]
     f 0 = pixel / 2
     f 1 = pixel * 3 / 2
     f i'
-      | idxInChip == 0  = pixel / 2 + pixel * fromIntegral (i' - (chip - 1) * 2 - 2) + fromIntegral (chip - 1) * 2 * bigPixelWidth + pixel / 2 + bigPixelWidth / 2 + bigPixelWidth
-      | idxInChip == 1  = pixel / 2 + pixel * fromIntegral (i' - (chip - 1) * 2 - 3) + fromIntegral (chip * 2) * bigPixelWidth + pixel
-      | idxInChip <= 78 = pixel / 2 + pixel * fromIntegral (i' - (chip * 2) - 0) + fromIntegral (chip * 2) * bigPixelWidth
-      | idxInChip == 79 = pixel / 2 + pixel * fromIntegral (i' - (chip * 2) - 1) + fromIntegral (chip * 2) * bigPixelWidth + pixel / 2 + bigPixelWidth / 2
+      | idxInChip == 0  = pixel * (fromIntegral i' + 3 * fromIntegral chip - 1 / 4)
+      | idxInChip <= 78 = pixel * (fromIntegral i' + 3 * fromIntegral chip + 1 / 2)
+      | idxInChip == 79 = pixel * (fromIntegral i' + 3 * fromIntegral chip + 5 / 4)
       | otherwise = error $ "wront coordinates" ++ show i'
       where
         (chip, idxInChip) = divMod i' chipw
 
     -- height
     g :: Int -> Double
-    g j = pixel / 2.0 + (pixel * fromIntegral j) + interModule * fromIntegral (module' j)
+    g j = pixel / 2.0 + (pixel * fromIntegral j) + interModule * fromIntegral module'
       where
+        module' :: Int
         module' = div j chiph
