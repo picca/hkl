67a62257ab83daac7cc146db997ee8f7346461d2
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2016-10-04 18:10:17 +0200

    [contrib][haskell] explicite imports

--- contrib/haskell/src/Hkl/XRD/Calibration.hs
+++ contrib/haskell/src/Hkl/XRD/Calibration.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Hkl.XRD.Calibration
        ( NptExt(..)
@@ -8,11 +7,31 @@ module Hkl.XRD.Calibration
        , calibrate
        ) where
 
-import Control.Monad.IO.Class
-import Data.ByteString.Char8
-import Data.List
-import Data.Text
+import Control.Monad.IO.Class (liftIO)
+import Data.List (foldl')
+import Data.Text (Text)
 import Data.Vector.Storable
+  ( Vector
+  , head
+  , concat
+  , fromList
+  , slice
+  , toList
+  )
+import Numeric.LinearAlgebra
+  ( Matrix
+  , (<>)
+  , atIndex
+  , ident
+  )
+import Numeric.GSL.Minimization
+  ( MinimizeMethod(NMSimplex2)
+  , minimizeV
+  )
+import Numeric.Units.Dimensional.Prelude (meter, radian, nano, (/~), (*~))
+import Pipes.Safe (MonadSafe(..), runSafeT, bracket)
+import Prelude hiding (head, concat, lookup, readFile, writeFile, unlines)
+
 import Hkl.C
 import Hkl.Detector
 import Hkl.H5
@@ -21,13 +40,7 @@ import Hkl.MyMatrix
 import Hkl.PyFAI.PoniExt
 import Hkl.Types
 import Hkl.XRD
-import Numeric.LinearAlgebra
-import Numeric.GSL.Minimization
--- import Graphics.Plot
-import Numeric.Units.Dimensional.Prelude (meter, radian, nano, (/~), (*~))
-import Prelude hiding (concat, lookup, readFile, writeFile, unlines)
 
-import Pipes.Safe (MonadSafe(..), runSafeT, bracket)
 
 #if !MIN_VERSION_hmatrix(0, 17, 0)
 (#>) :: Matrix Double -> Vector Double -> Vector Double
@@ -55,10 +68,10 @@ data XRDCalibration = XRDCalibration { xrdCalibrationName :: Text
                       deriving (Show)
 
 withDataItem :: MonadSafe m => File -> DataItem -> (Dataset -> m r) -> m r
-withDataItem hid (DataItem name _) = Pipes.Safe.bracket (liftIO acquire') (liftIO . release')
+withDataItem hid (DataItem name _) = bracket (liftIO acquire') (liftIO . release')
     where
       acquire' :: IO Dataset
-      acquire' = openDataset hid (Data.ByteString.Char8.pack name) Nothing
+      acquire' = openDataset hid (pack name) Nothing
 
       release' :: Dataset -> IO ()
       release' = closeDataset
@@ -75,8 +88,8 @@ getM f p i' = runSafeT $
       gamma <- get_position g' 0
       delta <- get_position d' i'
       wavelength <- get_position w' 0
-      let source = Source (Data.Vector.Storable.head wavelength *~ nano meter)
-      let positions = Data.Vector.Storable.concat [mu, komega, kappa, kphi, gamma, delta]
+      let source = Source (head wavelength *~ nano meter)
+      let positions = concat [mu, komega, kappa, kphi, gamma, delta]
       let geometry = Geometry K6c source positions Nothing
       let detector = ZeroD
       m <- geometryDetectorRotationGet geometry detector
@@ -125,22 +138,22 @@ poniEntryToList p = [ poniEntryRot1 p /~ radian
 
 calibrate :: XRDCalibration -> PoniExt -> Detector a -> IO PoniExt
 calibrate c (PoniExt p _) d =  do
-  let entry = Prelude.last p
-  let guess = Data.Vector.Storable.fromList $ poniEntryToList entry
+  let entry = last p
+  let guess = fromList $ poniEntryToList entry
   -- read all the NptExt
-  npts <- Prelude.mapM (readXRDCalibrationEntry d) (xrdCalibrationEntries c)
+  npts <- mapM (readXRDCalibrationEntry d) (xrdCalibrationEntries c)
   -- in order to improve computation speed, pre-compute the pixel coodinates.
 
   let (solution, _p) = minimizeV NMSimplex2 1E-16 3000 box (f (preCalibrate npts)) guess
   -- mplot $ drop 3 (toColumns p)
   print _p
-  return $ PoniExt [poniEntryFromList entry (Data.Vector.Storable.toList solution)] (MyMatrix HklB (ident 3))
+  return $ PoniExt [poniEntryFromList entry (toList solution)] (MyMatrix HklB (ident 3))
     where
       preCalibrate''' :: Detector a -> NptEntry -> NptEntry'
-      preCalibrate''' detector (NptEntry _ tth _ points) = (tth /~ radian, Prelude.map (coordinates detector) points)
+      preCalibrate''' detector (NptEntry _ tth _ points) = (tth /~ radian, map (coordinates detector) points)
 
       preCalibrate'' :: Npt -> Detector a -> Npt'
-      preCalibrate'' n detector = (nptWavelength n /~ meter, Prelude.map (preCalibrate''' detector) (nptEntries n))
+      preCalibrate'' n detector = (nptWavelength n /~ meter, map (preCalibrate''' detector) (nptEntries n))
 
       preCalibrate' :: NptExt a -> NptExt' a
       preCalibrate' (NptExt n m detector) = (preCalibrate'' n detector, m', detector)
@@ -148,38 +161,38 @@ calibrate c (PoniExt p _) d =  do
           (MyMatrix _ m') = changeBase m PyFAIB
 
       preCalibrate :: [NptExt a] -> [NptExt' a]
-      preCalibrate = Prelude.map preCalibrate'
+      preCalibrate = map preCalibrate'
 
       box :: Vector Double
-      box = Data.Vector.Storable.fromList [0.1, 0.1, 0.1, 0.01, 0.01, 0.01]
+      box = fromList [0.1, 0.1, 0.1, 0.01, 0.01, 0.01]
 
       f :: [NptExt' a] -> Vector Double -> Double
-      f ns params = Data.List.foldl' (f' rotation translation) 0 ns
+      f ns params = foldl' (f' rotation translation) 0 ns
         where
             rot1 = params `atIndex` 0
             rot2 = params `atIndex` 1
             rot3 = params `atIndex` 2
 
-            rotations = Prelude.map (uncurry fromAxisAndAngle)
-                        [ (Data.Vector.Storable.fromList [0, 0, 1], rot3 *~ radian)
-                        , (Data.Vector.Storable.fromList [0, 1, 0], rot2 *~ radian)
-                        , (Data.Vector.Storable.fromList [1, 0, 0], rot1 *~ radian)]
+            rotations = map (uncurry fromAxisAndAngle)
+                        [ (fromList [0, 0, 1], rot3 *~ radian)
+                        , (fromList [0, 1, 0], rot2 *~ radian)
+                        , (fromList [1, 0, 0], rot1 *~ radian)]
 
-            rotation = Data.List.foldl' (<>) (ident 3) rotations
+            rotation = foldl' (<>) (ident 3) rotations
 
             translation :: Vector Double
             translation = slice 3 3 params
 
       f' :: Matrix Double -> Vector Double -> Double -> NptExt' a -> Double
       f' rotation translation x ((_wavelength, entries), m, _detector) =
-        Data.List.foldl' (f'' translation r) x entries
+        foldl' (f'' translation r) x entries
           where
             r :: Matrix Double
             r = m <> rotation
 
       f'' :: Vector Double -> Matrix Double -> Double -> NptEntry'-> Double
       {-# INLINE f'' #-}
-      f'' translation r x (tth, pixels) = Data.List.foldl' (f''' translation r tth) x pixels
+      f'' translation r x (tth, pixels) = foldl' (f''' translation r tth) x pixels
 
       f''' :: Vector Double -> Matrix Double -> Double -> Double -> Vector Double -> Double
       {-# INLINE f''' #-}
